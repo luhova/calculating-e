b31adcd44f268e506c6703e763840a3484631c70
Gabriela Luhova <gabriela.luhova@gmail.com>
2016-07-01 10:58:51 +0300

    Add WorkerThread and CalculateE java files.

---
+++ CalculateE.java
@@ -0,0 +1,76 @@
+package CalculateE;
+
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+import java.util.Calendar;
+import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.*;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+
+public class CalculateE {
+    static int n_members;
+    static int n_threads;
+    static String fileName = "result.txt";
+    static boolean quiet = false;
+    static BigDecimal final_result = BigDecimal.valueOf(0);
+
+    public static void main(String[] args) {
+        for(int i = 0; i < args.length; i++) {
+            if(args[i].equals("-p")) {
+                n_members = new Integer(args[i+1]);
+                if(n_members <= 0) {
+                    System.out.println("Number of members must be bigger than 0.");
+                    return;
+                }
+            }
+            if(args[i].equals("-t")) {
+                n_threads = new Integer(args[i+1]);
+                if(n_threads <= 0) {
+                    System.out.println("Number of threads must be bigger than 0.");
+                    return;
+                    }
+            }
+            if(args[i].equals("-o")) {
+                fileName = args[i+1];
+            }
+            if(args[i].equals("-q")) {
+                quiet = true;
+            }
+        }
+        long start = Calendar.getInstance().getTimeInMillis();;
+
+        WorkerThread[] workers = new WorkerThread[n_members];
+        ExecutorService executor = Executors.newFixedThreadPool(n_threads);
+        ConcurrentHashMap<BigDecimal, BigDecimal> factorials = new ConcurrentHashMap<BigDecimal, BigDecimal>();
+
+        for(int i = 0; i < n_members; i++) {
+            WorkerThread worker = new WorkerThread(i, factorials, quiet);
+            executor.execute(worker);
+            workers[i] = worker;
+        }
+
+        executor.shutdown();
+        while(!executor.isTerminated()) { }
+
+        long end =  Calendar.getInstance().getTimeInMillis();;
+        for(int i = 0; i < workers.length; i++){
+            final_result = final_result.add(workers[i].getResult());
+        }
+
+        try(PrintWriter out = new PrintWriter(fileName)) {
+            out.println(final_result);
+        }catch(FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+
+        if(!quiet){
+            System.out.println("final_result: " + final_result);
+        }
+        System.out.printf("Task took " + (end - start) + " ms to complete.");
+    }
+}
---
+++ WorkerThread.java
@@ -0,0 +1,58 @@
+package CalculateE;
+
+import java.io.*;
+import java.math.*;
+import java.util.Calendar;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class WorkerThread implements Runnable {
+    private int nth_member;
+    private boolean quiet;
+    private BigDecimal result;
+    private ConcurrentHashMap<BigDecimal, BigDecimal> factorials;
+
+    public WorkerThread(int nth_member, ConcurrentHashMap<BigDecimal, BigDecimal> factorials, boolean quiet) {
+        this.nth_member = nth_member;
+        this.factorials = factorials;
+        this.quiet = quiet;
+    }
+
+    public BigDecimal getResult() {
+        return result;
+    }
+
+    public void run() {
+        long start = Calendar.getInstance().getTimeInMillis();
+        if(!quiet){
+            System.out.println(Thread.currentThread().getName() + " started calculating member number " + nth_member );
+        }
+        result = calc_nth_member(nth_member);
+        long end = Calendar.getInstance().getTimeInMillis();
+        if(!quiet){
+            System.out.println(Thread.currentThread().getName() + " stopped.");
+            System.out.println(Thread.currentThread().getName() + " Execution time was (millis): " + (end - start) + " ms.");
+            System.out.println(Thread.currentThread().getName() + " Result from thread is: " + result);
+        }
+    }
+
+    private BigDecimal factorial(BigDecimal number){
+        BigDecimal fact;
+        if(factorials.containsKey(number))
+            fact = factorials.get(number);
+        else{
+            if (number.compareTo(BigDecimal.ZERO) == 0 || number.compareTo(BigDecimal.ONE) == 0)
+                fact =  BigDecimal.ONE;
+            else
+                fact = number.multiply(factorial(number.subtract(BigDecimal.ONE)));
+        }
+        factorials.putIfAbsent(number, fact);
+        return fact;
+    }
+
+    private BigDecimal calc_nth_member(int k){
+        BigDecimal num = BigDecimal.valueOf(Math.pow(3*k, 2) + 1);
+        BigDecimal denom = factorial(BigDecimal.valueOf(3*k));
+        result = num.divide(denom, 15, RoundingMode.CEILING);
+        return result;
+    }
+}
